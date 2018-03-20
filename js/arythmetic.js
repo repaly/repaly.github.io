5c2068012017bdfbb9389852985507150efeb02f
repaly <dimsemin2008@yandex.ru>
2018-03-20 10:28:43 +0300

    первая стрелочка
    готова наполовину

--- js/arythmetic.js
+++ js/arythmetic.js
@@ -2,8 +2,14 @@ var number1 = randomize(6, 9);
 var result = randomize(11, 14);
 var number2 = result - number1;
 
+var startPoint = 22.4;
+var cmPercent = 2.75;
+
 var equationField = document.querySelector('.equation-field');
 equationField.innerHTML = "" + number1 + " + " + number2 + " = ?";
+
+var arrow1 = document.querySelector('#arrow');
+arrow1.style.width = number1 * cmPercent + "%";
 
 function randomize(min, max) {
   min = Math.ceil(min);
