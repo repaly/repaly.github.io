d41186f66a07c7fff7e94ceb89dcc3cddfdb7e5c
repaly <dimsemin2008@yandex.ru>
2018-03-20 21:37:45 +0300

    добавлены обработчики
    Добавил обработчики событий для инпутов

--- js/arythmetic.js
+++ js/arythmetic.js
@@ -8,12 +8,33 @@ var cmPercent = 2.75;
 $(".equation-field").html("" + number1 + " + " + number2 + " = ?");
 $(".arrow-container").width(number1 * cmPercent + "%");
 
+var $inputField = $(".input-number"); // поле ввода ответов
 
-var inputNumber = document.querySelector('.input-number');
+$inputField.click(function () {
+  if ($inputField.attr("contenteditable") == "true") {
+    $inputField.empty();
+  }
+});
 
+$inputField.keyup(function (event) {
+  var keyValue = event.key;
+
+  if ( !isNaN(parseInt(keyValue)) ) {
+    if (keyValue == number1) {
+      $inputField.removeAttr("contenteditable");
+      $inputField.css("text-shadow", "0 0 0 #7ec926");
+    } else {
+      $inputField.removeAttr("contenteditable");
+      $inputField.css("text-shadow", "0 0 0 #ff7c7c");
+      $inputField.attr("contenteditable", "true"); // удаляю и добавляю этот атрибут, чтобы поле не могло потенциально превратиться в огромный столб неверных чисел
+    }
+  } else {
+    $inputField.empty(); // если пользователь пытается ввести что-то, кроме цифр, то это удаляется моментально
+}
+});
 
 function randomize(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
 }
